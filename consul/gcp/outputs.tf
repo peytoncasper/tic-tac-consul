ae147fe303c6789c1c4bcbb8c852e44934e55e34
Peyton Casper <peytoncas@gmail.com>
2020-12-01 14:13:55 -0500

    Added better ouputs and updated header picture

---
+++ consul/gcp/outputs.tf
@@ -0,0 +1,3 @@
+output "web_ui" {
+    value = google_compute_address.consul.address
+}
--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,7 @@
-output "azure_function_hostname" {
-    value = module.functions.azure_function_domain
+output "Tic-Tac-Consul-Web-UI" {
+    value = "http://${module.gcp.web_ui}"
 }
 
-output "gcp_function_hostname" {
-    value = trimsuffix(trimprefix(module.functions.gcp_function_domain, "https://"), "tic-tac-consul-function")
-}
-
-output "aws_function_name" {
-    value = trimsuffix(trimprefix(module.functions.aws_function_domain, "https://"), "/dev")
+output "Consul-UI" {
+    value = "http://${module.gcp.web_ui}:8500"
 }
